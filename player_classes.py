c95c2e88af5e0c4ddc9f39c6ae5f833400079797
UgliestBikes <pauric.ward@gmail.com>
2020-01-15 20:41:34 +0000

    Add some stubbed item classes

---
+++ player_classes.py
@@ -0,0 +1,14 @@
+from item_classes import *
+
+
+class Player(object):
+    def __init__(self, name, health, inventory=Inventory()):
+        self.name = name
+        self.health = health
+        self.inventory = inventory
+
+    def take_damage(self, damage):
+        self.health -= damage
+
+    def heal(self, amount):
+        self.health += amount
