c348cef5a6320695d859d77aa696a88af7515224
Padraig Donnelly <padraigd@synopsys.com>
2020-01-16 13:12:54 +0000

    Changing variable name to avoid keyword.

--- lib/item.py
+++ lib/item.py
@@ -6,24 +6,24 @@ class Inventory(object):
         self.inventory = {}
 
     def add_item(self, item: Item) -> str:
-        self.inventory[item.id] = item
+        self.inventory[item.uuid] = item
         return f"{item.name} added!"
 
     def remove_item(self, item: Item) -> str:
-        i = self.inventory.get(item.id)
+        i = self.inventory.get(item.uuid)
         if i:
-            self.inventory.pop(item.id)
+            self.inventory.pop(item.uuid)
             return f"{item.name} removed!"
         else:
             return f"Inventory does not contain {item.name}"
 
     def has_item(self, item: Item) -> bool:
-        return self.inventory.get(item.id) != False
+        return self.inventory.get(item.uuid) != False
 
 
 class Item(object):
     def __init__(self, name, description):
-        self.id = uuid.uuid4()
+        self.uuid = uuid.uuid4()
         self.name = name
         self.description = description
 
