4f6b0116f2d4b07ac6fed71ec9e71d074e7535e4
Fado <pdonnelly19@qub.ac.uk>
2020-01-17 10:19:47 +0000

    Adding additional attributes to data classes.

---
+++ lib/living.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import List, Dict
+
+from lib.character_class import CharacterClass
+from lib.entity import *
+from lib.enums import Ability, Skill, Alignment
+
+
+@dataclass
+class Living(Entity):
+    name: str
+    description: str
+    character_class: CharacterClass 
+    level: int
+    background: str
+    race: str
+    alignment: Alignment
+    xp: int
+    abilities: Dict[Ability, int]
+    skills: List[Skill]
+    max_hp: int
+    current_hp: int
+    temporary_hp: int
+    armor_class: int
+    hd_value: int
+    hd_total: int
+    death_save_success: int = 0
+    death_save_failure: int = 0
+    inventory: Inventory = Inventory()
+
+    def get_modifier(self, ability: Ability) -> int:
+        value = self.abilities.get(ability)
+        print(self.abilities)
+        return (value - 10) // 2
+
+    def get_initiative(self) -> int:
+        raise NotImplementedError
+
+    def get_passive_perception(self) -> int:
+        raise NotImplementedError
+
+    def take_damage(self, damage) -> int:
+        self.current_hp -= damage
+        return self.current_hp
+
+    def heal(self, amount) -> int:
+        self.current_hp += amount
+        return self.current_hp
---
+++ test/living_test.py
@@ -0,0 +1,73 @@
+import unittest
+
+from lib.character_class import CharacterClass
+from lib.enums import Alignment, Ability, Skill
+from lib.living import Living
+
+
+class LivingTests(unittest.TestCase):
+
+    @staticmethod
+    def _createLiving():
+        return Living(
+            name = 'Fado',
+            description = 'Fado stands before you.',
+            character_class = None,
+            level = 4,
+            background = "",
+            race = "",
+            alignment = Alignment.CHAOTIC_EVIL,
+            xp = 0,
+            abilities = {
+                Ability.STRENGTH: 27,
+                Ability.DEXTERITY: 14,
+                Ability.CONSTITUTION: 12,
+                Ability.WISDOM: 10,
+                Ability.INTELLIGENCE: 7,
+                Ability.CHARISMA: 3
+            },
+            skills = [
+                Skill.PERCEPTION
+            ],
+            max_hp = 100,
+            current_hp = 100,
+            temporary_hp = 0,
+            armor_class = 18,
+            hd_value = 10,
+            hd_total = 4,
+        )
+
+    def testTakeDamage(self):
+        living = self._createLiving()
+        self.assertEqual(living.take_damage(10), 90)
+
+    def testHeal(self):
+        living = self._createLiving()
+        self.assertEqual(living.heal(10), 110)
+
+    def testGetStrengthModifier(self):
+        living = self._createLiving()
+        self.assertEqual(living.get_modifier(Ability.STRENGTH), 8)
+
+    def testGetDexModifier(self):
+        living = self._createLiving()
+        self.assertEqual(living.get_modifier(Ability.DEXTERITY), 2)
+
+    def testGetConhModifier(self):
+        living = self._createLiving()
+        self.assertEqual(living.get_modifier(Ability.CONSTITUTION), 1)
+    
+    def testGetWisModifier(self):
+        living = self._createLiving()
+        self.assertEqual(living.get_modifier(Ability.WISDOM), 0)
+
+    def testGetInthModifier(self):
+        living = self._createLiving()
+        self.assertEqual(living.get_modifier(Ability.INTELLIGENCE), -2)
+    
+    def testGetChaModifier(self):
+        living = self._createLiving()
+        self.assertEqual(living.get_modifier(Ability.CHARISMA), -4)
+
+if __name__ == '__main__':
+    unittest.main()
