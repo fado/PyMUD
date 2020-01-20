a63c20c6443574740e39a359601f66a341620afa
mgaston <matthewjgaston@gmail.com>
2020-01-20 18:46:22 +0000

    Creating dice rolling functionality, accepts amount of dice, amount of sides and final modifier.

--- lib/dice.py
+++ lib/dice.py
@@ -1,14 +1,24 @@
 from random import randint
+import re
 
 class Dice():
-    def roll(self, sides, number, modifier):
-        finalroll = 0
-        for x in range(0, number):
-            roll = (randint(1, sides))
-            finalroll = finalroll + roll
-        modifiedroll = finalroll + modifier
-        print(modifiedroll)
-        return modifiedroll
 
-d = Dice()
-d.roll()
+    def split():
+        numbers = re.split('[d+]', roll)
+        return numbers
+
+    @static def roll(self, numbers):
+                for x in numbers:
+                    try:
+                        int(x)
+                    except ValueError:
+                        pass
+                number = numbers[0]
+                sides = numbers[1]
+                modifier = numbers[2]
+                finalroll = 0
+                for x in range(0, number):
+                    roll = (randint(1, sides))
+                    finalroll = finalroll + roll
+                modifiedroll = finalroll + modifier
+                return modifiedroll
---
+++ lib/split.py
@@ -0,0 +1,5 @@
+import re
+
+def split():
+    numbers = re.split('[d+]', roll)
+    return numbers
