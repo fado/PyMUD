d55b6b0fe0942f46b4a973fd7c398683071be4e4
mgaston <matthewjgaston@gmail.com>
2020-01-22 17:43:18 +0000

    Creating dice rolling functionality, accepts amount of dice, amount of sides and final modifier.

--- lib/dice.py
+++ lib/dice.py
@@ -1,9 +1,8 @@
 from random import randint
 
-    def roll(self, number, sides, modifier):
-        finalroll = 0
-        for x in range(0, number):
-            roll = (randint(1, sides))
-            finalroll = finalroll + roll
-        modifiedroll = finalroll + modifier
-        return modifiedroll
+def roll(self, number, sides, modifier):
+    finalroll = 0
+    for x in range(number):
+        roll = (randint(1, sides))
+        finalroll += roll
+    return finalroll + modifier
